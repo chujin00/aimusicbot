447bc3cc2b961ec240512d0c477660ed39547bc2
胡楚衿 <hcj@shangy.com>
2025-02-05 16:55:16 +0800

    Initial Commit

---
+++ src/components/chat/chatBubble/YoutubePlayer.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="youtube-player">
+    <iframe
+      :src="videoUrl"
+      width="100%"
+      height="315"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    videoId: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    videoUrl() {
+      return `https://www.youtube.com/embed/${this.videoId}`;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.youtube-player {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
